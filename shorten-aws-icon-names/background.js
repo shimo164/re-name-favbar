63c307ae2a93a82c85648ae133cbdd9a564665be
shimo164 <shimo164@gmail.com>
2023-05-18 06:02:12 +0900

    To avoid errors on the urls without permission, add the check in background.js

--- shorten-aws-icon-names/background.js
+++ shorten-aws-icon-names/background.js
@@ -1,8 +1,11 @@
 init = (tab) => {
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id, allFrames: true },
-    files: ['content_script.js'],
-  });
+  // Check if the tab URL matches the AWS Console pattern
+  if (/^https?:\/\/([a-z0-9-]+\.)*console\.aws\.amazon\.com\//.test(tab.url)) {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id, allFrames: true },
+      files: ['content_script.js'],
+    });
+  }
 };
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
