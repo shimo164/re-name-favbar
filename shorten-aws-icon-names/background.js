c998a3fe6aeb08792f5435047453f974333676f0
shimo164 <shimo164@gmail.com>
2023-05-18 23:08:33 +0900

    fix:unchecked runtime error with no id. error handling of tab closed before the code run

--- shorten-aws-icon-names/background.js
+++ shorten-aws-icon-names/background.js
@@ -1,17 +1,24 @@
 init = (tab) => {
+  if (!tab) {
+    console.log('Tab not found');
+    return;
+  }
+
   // Check if the tab URL matches the AWS Console pattern
   if (/^https?:\/\/([a-z0-9-]+\.)*console\.aws\.amazon\.com\//.test(tab.url)) {
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id, allFrames: true },
-      files: ['content_script.js'],
-    });
+    chrome.scripting
+      .executeScript({
+        target: { tabId: tab.id, allFrames: true },
+        files: ['content_script.js'],
+      })
+      .catch((error) => {
+        console.log(`Error executing script: ${error}`);
+      });
   }
 };
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
-  if (changeInfo.status === 'complete') {
-    chrome.tabs.get(tabId, (tab) => {
-      init(tab);
-    });
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'complete' && tab.status === 'complete') {
+    init(tab);
   }
 });
