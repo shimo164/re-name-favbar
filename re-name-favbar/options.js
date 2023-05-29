ac0567dff4fe68159e4b38b6d8a923f19f33f0ea
shimo164 <shimo164@gmail.com>
2023-05-30 07:01:20 +0900

    feat: Add toggle enable, toggle icon only mode in options page

--- re-name-favbar/options.js
+++ re-name-favbar/options.js
@@ -1,3 +1,56 @@
+const defaultMapping = {
+  'API Gateway': 'API GW',
+  'Application Discovery Service': 'ADS',
+  'AppStream 2.0': 'AppStream',
+  'AWS Auto Scaling': 'ASG',
+  'Certificate Manager': 'ACM',
+  CloudFormation: 'CFN',
+  CloudFront: 'CF',
+  CloudHSM: 'HSM',
+  CloudSearch: 'CS',
+  CloudWatch: 'CW',
+  'Database Migration Service': 'DMS',
+  'Direct Connect': 'DX',
+  DynamoDB: 'DDB',
+  'Elastic Beanstalk': 'EB',
+  'Elastic Container Registry': 'ECR',
+  'Elastic Container Service': 'ECS',
+  'Elastic Kubernetes Service': 'EKS',
+  'IoT Device Management': 'IoT DM',
+  'Key Management Service': 'KMS',
+  'Managed Apache Airflow': 'MAA',
+  'Resource Access Manager': 'RAM',
+  'Route 53': 'R53',
+  'Service Catalog': 'SC',
+  'Simple Notification Service': 'SNS',
+  'Simple Queue Service': 'SQS',
+  'Step Functions': 'SF',
+  'Storage Gateway': 'SG',
+  'Systems Manager': 'SSM',
+  'Trusted Advisor': 'TA',
+  'WAF & Shield': 'WAF',
+};
+
+// Initialize the toggles with their saved states or defaults.
+chrome.storage.sync.get(['extensionEnabled', 'emptyAllNames'], function (data) {
+  document.getElementById('extensionToggle').checked =
+    data.extensionEnabled !== false; // default: true
+  document.getElementById('emptyNamesToggle').checked =
+    data.emptyAllNames === true; // default: false
+});
+
+// Save the state of the toggles whenever they are changed.
+document
+  .getElementById('extensionToggle')
+  .addEventListener('change', function () {
+    chrome.storage.sync.set({ extensionEnabled: this.checked });
+  });
+document
+  .getElementById('emptyNamesToggle')
+  .addEventListener('change', function () {
+    chrome.storage.sync.set({ emptyAllNames: this.checked });
+  });
+
 // Attach event listeners to buttons for saving and resetting options.
 document.getElementById('saveButton').addEventListener('click', saveOptions);
 document.getElementById('resetButton').addEventListener('click', resetOptions);
