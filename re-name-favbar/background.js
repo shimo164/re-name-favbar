be15f1ec5031fdf868d8f609c9c07121d365a66b
shimo164 <shimo164@gmail.com>
2023-05-29 21:43:36 +0900

    fix: default text mapping work properly

--- re-name-favbar/background.js
+++ re-name-favbar/background.js
@@ -1,3 +1,43 @@
+chrome.runtime.onInstalled.addListener((details) => {
+  if (details.reason === 'install') {
+    const defaultMapping = {
+      'API Gateway': 'API GW',
+      'Application Discovery Service': 'ADS',
+      'AppStream 2.0': 'AppStream',
+      'AWS Auto Scaling': 'ASG',
+      'Certificate Manager': 'ACM',
+      CloudFormation: 'CFN',
+      CloudFront: 'CF',
+      CloudHSM: 'HSM',
+      CloudSearch: 'CS',
+      CloudWatch: 'CW',
+      'Database Migration Service': 'DMS',
+      'Direct Connect': 'DX',
+      DynamoDB: 'DDB',
+      'Elastic Beanstalk': 'EB',
+      'Elastic Container Registry': 'ECR',
+      'Elastic Container Service': 'ECS',
+      'Elastic Kubernetes Service': 'EKS',
+      'IoT Device Management': 'IoT DM',
+      'Key Management Service': 'KMS',
+      'Managed Apache Airflow': 'MAA',
+      'Resource Access Manager': 'RAM',
+      'Route 53': 'R53',
+      'Service Catalog': 'SC',
+      'Simple Notification Service': 'SNS',
+      'Simple Queue Service': 'SQS',
+      'Step Functions': 'SF',
+      'Storage Gateway': 'SG',
+      'Systems Manager': 'SSM',
+      'Trusted Advisor': 'TA',
+      'WAF & Shield': 'WAF',
+    };
+    chrome.storage.sync.set({ textMappings: defaultMapping }, function () {
+      console.log('Default textMappings set');
+    });
+  }
+});
+
 init = (tab) => {
   if (!tab) {
     console.log('Tab not found');
--- re-name-favbar/options.js
+++ re-name-favbar/options.js
@@ -1,47 +1,6 @@
-// Default mapping between AWS service names and their renamed versions.
-const defaultMapping = {
-  'API Gateway': 'API GW',
-  'Application Discovery Service': 'ADS',
-  'AppStream 2.0': 'AppStream',
-  'AWS Auto Scaling': 'ASG',
-  'Certificate Manager': 'ACM',
-  CloudFormation: 'CFN',
-  CloudFront: 'CF',
-  CloudHSM: 'HSM',
-  CloudSearch: 'CS',
-  CloudWatch: 'CW',
-  'Database Migration Service': 'DMS',
-  'Direct Connect': 'DX',
-  DynamoDB: 'DDB',
-  'Elastic Beanstalk': 'EB',
-  'Elastic Container Registry': 'ECR',
-  'Elastic Container Service': 'ECS',
-  'Elastic Kubernetes Service': 'EKS',
-  'IoT Device Management': 'IoT DM',
-  'Key Management Service': 'KMS',
-  'Managed Apache Airflow': 'MAA',
-  'Resource Access Manager': 'RAM',
-  'Route 53': 'R53',
-  'Service Catalog': 'SC',
-  'Simple Notification Service': 'SNS',
-  'Simple Queue Service': 'SQS',
-  'Step Functions': 'SF',
-  'Storage Gateway': 'SG',
-  'Systems Manager': 'SSM',
-  'Trusted Advisor': 'TA',
-  'WAF & Shield': 'WAF',
-};
-
 // Attach event listeners to buttons for saving and resetting options.
 document.getElementById('saveButton').addEventListener('click', saveOptions);
 document.getElementById('resetButton').addEventListener('click', resetOptions);
-
-// When the extension is installed, load the default mappings into storage and display them on the options page.
-chrome.runtime.onInstalled.addListener(function () {
-  chrome.storage.sync.set({ textMappings: defaultMapping }, function () {
-    populateOptions();
-  });
-});
 
 // When the options page is loaded, display the current mappings.
 document.addEventListener('DOMContentLoaded', populateOptions);
